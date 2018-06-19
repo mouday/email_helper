e90fe5b77eb9023614709018f610b742a401f075
彭世瑜 <psy068@qimingpian.com>
2018-06-19 17:19:41 +0800

    发布第三版

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="email_helper",
-    version="0.0.2",
+    version="0.0.3",
     author="Pengshiyu",
     author_email="pengshiyuyx@gmail.com",
     description="email helper for simple send email and receive email of python",
